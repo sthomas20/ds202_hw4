c0f6d740fa805dce16287172a236efe81419f06b
sthomas20 <sthomas8@iastate,edugit config --global user.email sthomas8@iastate.edu>
2020-03-04 12:00:51 -0600

    updated HW4

--- hw4.R
+++ hw4.R
@@ -34,32 +34,40 @@ head(complete, n= 3)
 
 #Question 4 ?
 day21<- filter(complete, Time == 21)
-one<- tmp <- ggplot(day21, aes(x = reorder(Diet, weightgain, FUN = median), y = weightgain)) + geom_boxplot() + expand_limits(y=0) + xlab("Weight Gain") + ylab("Amount of Weight Gain by day 21") 
+one<- tmp <- ggplot(day21, aes(x = reorder(Diet, weightgain, FUN = median), y = weightgain)) + geom_boxplot() + expand_limits(y=0) + xlab("Diet") + ylab("Weight Gain") + ggtitle("Amount of weight gained over time")
 one<- one + theme(plot.title = element_text(hjust=0.5))
 one
 
-#Question 5
-#a
-#complete$weightgain = as.factor(complete$weightgain)
-str(complete)
+#The boxplot I think clear shows that the increase in weight gain as the value of your diet again increases based on looking at the median but breaks after diet 2. I see this because the median for diet 3 is larger than diet 4. You can also see that the diet there has the highest weight gain based on the IQR and the median shown. 
 
-ggplot(complete, aes(x=Time, y=weightgain, group=Chick, colour= Chick)) + geom_line() + geom_point() + guides(colour = guide_legend(reverse=T))
+#Question 5
+
+maxWeight <- max(complete$weightgain)
+maxWeight
+
+minWeight<- min(complete$weightgain)
+minWeight
+
+weightGain<- complete %>% 
+  group_by(Chick) %>%
+  filter(weightgain == maxWeight | weightgain == minWeight)
+print(weighGain)
+
+ggplot(weightGain, aes(x=Time, y=weight, colour= Chick)) + geom_line() + geom_point() + guides(colour = guide_legend(reverse=T)) + facet_wrap(~Diet) + xlab("Time") + ylab("Weight") + ggtitle("Most/Least weight gained Chick")
 
 
 #Question 6
-least<- complete[complete[, 'Time'] == 0, ]
+chicks<- complete %>% 
+  group_by(Chick) %>%
+  filter(Chick == 3 | Chick == 35)
 
-least<- least[order(least$weight), ] 
-View(least)
-
-most<- complete[complete[, 'Time'] == 21, ]
-most<- most[order(most$weight), ]
-most
-
+ggplot(chicks, aes(x=Time, y=weight, colour= Chick)) + geom_line() + geom_point() + ggtitle("Chick # 3 and 35 growth trajectories") + xlab("Time") + ylab("Weight")
 
 
 #Question 7
+ggplot(chicks, aes(x=Time, y=weight, colour= Chick)) + geom_line() + geom_point() + ggtitle("Chick # 3 and 35 growth trajectories") + xlab("Time") + ylab("Weight") + geom_smooth(method="lm") + geom_smooth(data=complete, aes(x = Time, y = weight, colour="Trend Line"), method="lm")
 
+  
 
 
 
